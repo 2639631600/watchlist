e999411bfd941591f672028b7f1a6a7acbe227d8
2639631600 <2639631600@qq.com>
2021-04-08 01:19:14 +0800

    faker

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template
 from flask import escape,url_for
+from flask import Flask
+
 app = Flask(__name__)
 
 
---
+++ myfaker.py
@@ -0,0 +1,7 @@
+from faker import Faker
+from faker.providers import internet,company
+fake = Faker('zh_CN')
+fake.add_provider(internet)
+fake.add_provider(company)
+for _ in range(10):
+    print(fake.name(), fake.ascii_free_email(), fake.company())
